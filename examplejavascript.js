36cd1154fcc79dc00a944aeac50e56ff4c9ab6ad
KanishkarCse <126953472+KanishkarCse@users.noreply.github.com>
2023-03-10 21:33:19 +0530

    Create examplejavascript.js

---
+++ examplejavascript.js
@@ -0,0 +1,18 @@
+pleTry this code »
+var x; 
+ 
+x = 10;
+alert(++x); 
+alert(x);  
+ 
+x = 10;
+alert(x++); 
+alert(x);  
+ 
+x = 10;
+alert(--x); 
+alert(x);   
+ 
+x = 10;
+alert(x--); 
+alert(x);   
